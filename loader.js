7ef3f6f01861349e1fd3203c76a4ac6fc8e2fa87
Fernando Serboncini <fserb@fserb.com.br>
2010-05-14 16:29:45 +0200

    multi text select

--- loader.js
+++ loader.js
@@ -1,20 +1,21 @@
+/*
+we first load jquery, then google API then language API then our script.
+*/
+
 if (!window['apiLoaded']) {
   window.apiLoaded = function() {
     google.load("language", "1", { "callback" : function() {
 			$(document).ready(function() {  
 				var sc = document.createElement('script');
-				sc.src = '../tuntext.js?t=' + Date();
+				sc.src = 'http://localhost:8080/tuntext.js';
 				sc.type = "text/javascript";
 				document.getElementsByTagName('body')[0].appendChild(sc);
 			})}});
 	};
 }
 	
-if (!window['jQuery']) {
-  var sc = document.createElement('script');
-  sc.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js';
-  sc.type = "text/javascript";
-	sc.onload = function() {
+if (!window['loadGoogle']) {
+	loadGoogle = function() {
 		if (!window['google']) {
 			var s = document.createElement('script');
 			s.src = 'http://www.google.com/jsapi?callback=apiLoaded';
@@ -22,8 +23,18 @@ if (!window['jQuery']) {
 			document.getElementsByTagName('body')[0].appendChild(s);
 		} else {
 			apiLoaded();
-		};
+		}
 	};
+}
+
+
+if (!window['jQuery']) {
+  var sc = document.createElement('script');
+  sc.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js';
+  sc.type = "text/javascript";
+	sc.onload = loadGoogle;
   document.getElementsByTagName('body')[0].appendChild(sc);
+} else {
+	loadGoogle();
 }
 
--- tuntext.js
+++ tuntext.js
@@ -1,8 +1,11 @@
 /*
 TODO:
 - language select box on the top right corner
-- reload original page on top right
+- black covers whole page
+- strip page content
 */
+
+var basepath = "http://localhost:8080/"
 
 var special = new RegExp(/([ \f\n\r\t\v\u00A0\u2028\u2029,:;\-~\.\(\)\[\]\{\}\\\/?\!]+)/);
 
@@ -14,17 +17,19 @@ var langs = {
 var curlang = "";
 
 makePopup = function(text, x, y) {
-	$("#pop span").text(text);
-	var pop = $("#pop");
+	$("#tuntext_pop span").text(text);
+	var pop = $("#tuntext_pop");
 	pop.css("top", x - pop.innerHeight() - 10 + "px");
 	pop.css("left", y - pop.innerWidth()/2 +"px");
 	pop.show();
 	var w = pop.innerWidth();
-	$("#pop .popab, #pop .popa").css("left", w/2 - 6);
+	$("#tuntext_pop .tuntext_popab, #tuntext_pop .tuntext_popa")
+		.css("left", w/2 - 6);
 }
 
 wordClick = function(ev) {
 	console.log("click");
+	ev.stopPropagation();
 	if (ev.button != 0) {
 		return;
 	}
@@ -39,15 +44,76 @@ wordClick = function(ev) {
 	});
 }
 
-makeBlocks = function(target) {
-	var text = target.text();
+findChildIndex = function(x, y) {
+	var found = -1;
+	$("#tuntext_content a").each(function(index) {
+		var pos = $(this).position();
+		if ( x > pos.left && 
+				 y > pos.top &&
+				 (x - pos.left) < $(this).width() &&
+				 (y - pos.top) < $(this).height()) {
+			found = index;
+		}
+	});
+	return found;
+}
+
+var draggingFrom = -1;
+
+wordUp = function(ev) {
+	if (draggingFrom == -1) return;
+	var idx = findChildIndex(ev.pageX, ev.pageY);
+	console.log("up: " + draggingFrom + " - " + (idx+1));
+	
+	var txt = "";
+	$("#tuntext_content a").slice(draggingFrom, idx+1).each(function() {
+		txt += $(this).text() + " ";
+	});
+	txt = $.trim(txt);
+	var first = $("#tuntext_content a").eq(draggingFrom);
+	var last = $("#tuntext_content a").eq(idx);
+	var pos = first.position();
+	var width = Math.max(first.innerWidth(),
+											 last.position().left + last.innerWidth() - pos.left);
+	google.language.translate(txt, curlang, "en", function(res) {
+		$("#tuntext_content a").removeClass("selected");
+		makePopup(res.translation,
+							pos.top,
+							pos.left + width/2);
+	});
+	draggingFrom = -1;
+};
+
+wordMove = function(ev) {
+	if (draggingFrom == -1) return;
+	var idx = findChildIndex(ev.pageX, ev.pageY);
+
+	$("#tuntext_content a").removeClass("selected");
+	if (idx != -1) {
+		$("#tuntext_content a").slice(draggingFrom, idx+1).addClass("selected");
+	}
+
+
+};
+
+wordDown = function(ev) {
+	ev.preventDefault();
+	var idx = findChildIndex(ev.pageX, ev.pageY);
+	if (idx != -1) {
+		draggingFrom = idx;
+	}
+};
+
+LoadText = function(text) {
+	text = $.trim(text);
 	google.language.detect(text.substr(0, 128), function(res) {
+		var target = $("#tuntext_content");
 		var l = langs[res.language];
 		if (!l) {
 			l = [ res.language, "" ];
 		}
 		curlang = res.language;
-		$("#sourcelang").text(l[0]);
+		//$("#sourcelang").text(l[0]);
 		var split = text.split(special);
 		target.empty();
 		for(var i = 0; i < split.length; ++i) {
@@ -59,6 +125,7 @@ makeBlocks = function(target) {
 				var enter = s.split(/(\n)/);
 				for (var j = 0; j < enter.length; ++j) {
 					if (enter[j] == "\n") {
+
 						target.append("<p>");
 					} else {
 						target.append(enter[j]);
@@ -75,69 +142,65 @@ makeBlocks = function(target) {
 				target.append(d);
 			}
 		}
+		target.show();
 	});
 };
 
-processText = function(node) {
-	var skip = 0;
-	var split = node.split(special);
-	for(var i = 0; i < split.length; ++i) {
-		var s = split[i];
-		if (s.length == 0) {
-			continue;
-		}
-		if (s.match(special)) {
-			var enter = s.split(/(\n)/);
-			for (var j = 0; j < enter.length; ++j) {
-				if (enter[j] == "\n") {
-					target.append("<p>");
-					skip += 1;
-				} else {
-					target.append(enter[j]);
-				}
-			}
-		} else {
-			console.log(s);
-			var d = $("<a>" + s + "</a>");
-			if (true) {
-				d.attr("href", "http://" + s);
-			} else {
-				d.attr("href", "");
-			}
-			d.click(wordClick);
-			target.append(d);
-			skip += 1;
-		}
+Prepare = function() {
+	if ($("#tuntext").length != 0) {
+		$("#tuntext_pop").hide();
+		$("#tuntext").show();
+		return;
 	}
-	return 0;
-};
 
-prepareElements = function(node) {
-	var skip = 0;
-  if (node.nodeType == 3) {
-		skip += processText(node);
-  }
-  else if (node.nodeType == 1 && 
-					 node.childNodes && 
-					 !/(script|style)/i.test(node.tagName)) {
-		for (var i = 0; i < node.childNodes.length; ++i) {
-			i += prepareElements(node.childNodes[i]);
-		}
-  }
-  return skip;
-};
+	var style = $("<link>");
+	style.attr("rel", "stylesheet");
+	style.attr("type", "text/css");
+	style.attr("href", basepath + "tuntext.css");
+	$("body").append(style);
 
-clearPage = function() {
-	$("a").removeAttr("href");
-	$("*").each(function() {
-		$(this).unbind("click");
+	var main = $("<div id='tuntext'></div>");
+	main.click(function() {
+		$(this).hide();
 	});
+	$("body").append(main);
+
+	var content = $("<div id='tuntext_content'></div>");
+	content.hide();
+	content.mousedown(wordDown);
+	content.mouseup(wordUp);
+	content.mousemove(wordMove);
+	content.click(function(ev) {
+		ev.stopPropagation();
+		$("#tuntext_pop").hide();
+	});
+	main.append(content);
+
+	var pop = $("<div id='tuntext_pop'><span></span>" +
+							"<div class='tuntext_popab'></div>" +
+							"<div class='tuntext_popa'></div></div>");
+	pop.click(function(ev) {
+		ev.stopPropagation();
+		$("#tuntext_pop").hide();
+	});
+	main.append(pop);
+}
+
+Tuntext = function(text) {
+	Prepare();
+	LoadText(text);
 };
 
+RunSelection = function() {
+	var text = window.getSelection().toString();
+	if (!text) {
+		//alert("No text selected");
+		//return;
+		text = "Teheran (nhz) ~ Ein Kleriker aus der schönen Stadt Teheran, welche des öfteren unter Erdbeben zu leiden hat, behauptete doch, dass öffentlich zur Schau getragene Busen dafür verantwortlich seien.\nAufgeschreckt gaben wir unseren Seismologen sofort den Auftrag diesen, nicht von der Hand zu weisenden Zusammenhang, wissenschaftlich zu untersuchen. In Australien und Kanada wurden Trägerinnen eines Busens aufgefordert diesen zu zeigen. Die Nadeln der Seismologen schlugen aber keinen Millimeter aus. Auch die Redakteure der NHZ, welche ihre Gattinnen oder Lebensgefährtinnen baten bei der Wahrheitsfindung zu helfen, konnten keine Erbeben feststellen.\nMit ganz viel Vertrauen in unsere Untersuchungen können wir allen Frauen mitteilen\n\nBusen haben vielleicht auf die Erdbeerernte Einfluss,\naber garantiert nicht auf Erdbeben!";
+	} 
 
-run = function() {
-	clearPage();
-	prepareElements($("body").get(0));
+	Tuntext(text);
+
 };
 
-run();
+RunSelection();
